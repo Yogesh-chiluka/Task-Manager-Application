ab061f14612015f227a8e91cbfd225acb864817e
Yogesh Chiluka <72222615+Yogesh-chiluka@users.noreply.github.com>
2024-09-07 18:09:44 +0000

    Add tailwindcss custom colors for UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,17 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueBar: '#0275FE',
+        blueCard: '#CCE7FB',
+        cardHeader: '#3a98FB',
+        delete:'#FF575E',
+        edit:'#3898FB',
+        details:'#',
+        // Add more custom colors here
+      },
+    },
   },
   plugins: [],
 }
